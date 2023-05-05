678b2fd01786ec8c20bd9897866d3f4c39e5ae7c
shilpa <shilparanjan95@gmail.com>
2023-05-05 15:45:25 +0530

    first commit

---
+++ medicine/medicine/src/test/java/com/medicare/medicine/MedicareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicare.medicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
